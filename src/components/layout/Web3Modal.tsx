23a139a8e2c4a48c3ab06176f73184065ae7fc2d
Doug Richar <drichar@gmail.com>
2023-06-08 02:53:54 -0400

    chore: use Algo logo in Web3Modal

--- src/components/layout/Web3Modal.tsx
+++ src/components/layout/Web3Modal.tsx
@@ -10,7 +10,7 @@ export default function Web3Modal() {
         name: 'Algorand + WalletConnect 2.0 Example App',
         description: 'Demonstrates how to use WalletConnect 2.0 with Algorand',
         url: 'https://algorand-wc2.vercel.app',
-        icons: ['https://algorand-wc2.vercel.app/logo.png']
+        icons: ['https://algorand-wc2.vercel.app/algo.svg']
       }}
       modalOptions={{
         themeMode: 'light',
