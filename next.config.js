a0bffcfc47d4baac0c075448caa91afce92b26ff
Doug Richar <drichar@gmail.com>
2023-06-08 01:51:37 -0400

    fix: "Module not found" errors in local development

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    // @see https://github.com/WalletConnect/walletconnect-monorepo/issues/1908#issuecomment-1487801131
+    config.externals.push('pino-pretty', 'lokijs', 'encoding', 'utf-8-validate', 'bufferutil')
+    return config
+  },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'tailwindui.com',
+        hostname: 'walletconnect.com',
         port: ''
       }
     ]
